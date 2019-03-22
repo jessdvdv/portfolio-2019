27bacb6510fef28292247e29679df11ca8e7e1ae
Jessica Weber <hello@jessicaweber.io>
2019-03-21 20:57:27 -0700

    updated images. contact page and 404 page.

--- wp-content/themes/wpex-adapt/404.php
+++ wp-content/themes/wpex-adapt/404.php
@@ -10,14 +10,15 @@
 get_header(); ?>
 
 	<header id="page-heading">
-		<h1 class="page-title"><?php _e( '404 Error', 'wpex-adapt' ); ?></h1>		
+		<h1 class="page-title">Oops, it's a goof! Looks like that page doesn't exist!</h1>		
 	</header><!-- #page-heading -->
 	
+	<img src="http://localhost:8888/portfolio-2019/wp-content/uploads/2019/03/IMG_8954.jpg" alt"Jessica is a goof"/>
+
 	<div class="content-area clearfix">
 		<div class="entry clearfix">
-			<p><?php _e( 'Sorry, the page you were looking for could not be found.', 'wpex-adapt' ); ?></p>
+			
 		</div><!-- .entry -->
 	</div><!-- .content-area -->
 	
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
